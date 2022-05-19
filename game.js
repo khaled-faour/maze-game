a798364d266517b6091fac293a00b50b05b48955
Khaled Faour <khaledfaour2@gmail.com>
2022-05-20 01:13:32 +0300

    Added Start/End event listeners

--- game.js
+++ game.js
@@ -11,4 +11,19 @@ function startGame(){
     // Get end Boc
     const end = document.querySelector('#end')
 
+    // Start click event listener
+    start.addEventListener('click', startClick)
+
+    // End mouseover event listener
+    end.addEventListener('mouseover', endOver)
+
+    // Function called when start box clicked
+    function startClick(){
+
+    }
+
+    // Function called when mouse over end box
+    function endOver(){
+
+    }
 }
