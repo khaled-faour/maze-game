d2aa7c97755c464a059966ea89c56babfb0e0f97
Khaled Faour <khaledfaour2@gmail.com>
2022-05-20 00:55:40 +0300

    Added elements querySelectors

--- game.js
+++ game.js
@@ -2,5 +2,13 @@
 indow.addEventListener('load', startGame);
 
 function startGame(){
+    // Get boundary elements
+    const boundaries = document.querySelectorAll('.boundary')
+    
+    // Get start box  
+    const start = document.querySelector('#start')
+    
+    // Get end Boc
+    const end = document.querySelector('#end')
 
 }
